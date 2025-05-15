704a853051aedfa499b26911faa599b3561ad953
unisol1020 <unisol1020@icloud.com>
2025-05-15 21:30:20 +0300

    Remove unused exports from Testimonials index file to streamline component structure

--- src/components/testimonials/index.ts
+++ src/components/testimonials/index.ts
@@ -1,6 +1,1 @@
-export * from "./TestimonialAuthor";
-export * from "./TestimonialCarousel";
-export * from "./TestimonialQuote";
 export * from "./Testimonials";
-export * from "./TestimonialsHeader";
-export * from "./types";
